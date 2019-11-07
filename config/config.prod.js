af10dc7f42b87fcedd1bade51d34608577c8c20b
lijianhu1 <403056352@qq.com>
2019-11-07 16:32:57 +0800

    添加生产环境配置

--- config/config.prod.js
+++ config/config.prod.js
@@ -6,33 +6,12 @@
  * @param {Egg.EggAppInfo} appInfo app info
  */
 module.exports = appInfo => {
-  /**
-   * built-in config
-   * @type {Egg.EggAppConfig}
-   **/
-  const config = exports = {};
-
-  // use for cookie sign key, should change to your own and keep security
-  config.keys = appInfo.name + '_1572941540267_978';
-  config.passportLocal = {
-    username: '',
-    password: '',
-  };
-  // add your middleware config here
-  config.middleware = [ 'isLogin', 'notfoundHandler', 'errorHandler' ];
-  config.security = {
-    xframe: {
-      enable: true,
-    },
-    csrf: {
-      headerName: 'csrfToken',
-    },
-  };
+  const config = exports;
   config.sequelize = {
     dialect: 'mysql',
     host: '127.0.0.1',
     port: '3306',
-    database: 'imian',
+    database: 'ivva',
     username: 'root',
     password: 'root',
     define: { // model的全局配置
